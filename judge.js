d43c0557bfd4cd3ca6abb2858effe2c72e7d282a
LeGougaudT <tom.legougaud@gmail.com>
2016-01-27 14:53:08 +0100

    creation of methods for rewards

--- judge.js
+++ judge.js
@@ -2,15 +2,27 @@ var J = {};
 (function(global) {
   "use strict";
   
-  var Judge = function(env) {
+  var Judge = function(env, envTeAgent, envSpAgent1, envSpAgent2, envSpAgent3) {
   this.env = env; // store pointer to environment
+  this.envTeAgent = envTeAgent; // store pointer to environment
+  this.envSpAgent1 = envSpAgent1; // store pointer to environment
+  this.envSpAgent2 = envSpAgent2; // store pointer to environment
+  this.envSpAgent3 = envSpAgent3; // store pointer to environment
   this.reset();
 };
 Judge.prototype = {
   reset: function() {
       
   },
-  getReward:function(a1, a2, a3){
+  getRewardTemporal:function(){
+      var reward = 0;
+      var state = this.envTeAgent.getState();
+      var freqAction = state[0];
+      var freqHumanAgent = state[1];
+      reward = freqHumanAgent;
+      return reward; 
+  },
+  getRewardSpatial:function(){
       var reward;
       if(a1 === 0){
           reward = 5;
