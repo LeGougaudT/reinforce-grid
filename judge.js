3bd59f310e8a9176b933a6a0e3c636f2a1493143
LeGougaudT <tom.legougaud@gmail.com>
2016-01-28 22:29:49 +0100

    test with global color in judge, need to patch

--- judge.js
+++ judge.js
@@ -8,6 +8,7 @@ var J = {};
         this.envSpAgent1 = envSpAgent1; // store pointer to environment
         this.envSpAgent2 = envSpAgent2; // store pointer to environment
         this.envSpAgent3 = envSpAgent3; // store pointer to environment
+        this.TAB_MAX = 50;
         this.reset();
     };
     Judge.prototype = {
@@ -39,9 +40,9 @@ var J = {};
         },
         getRewardSpatial:function(a1,a2,a3){
             this.pushValue(a1,a2,a3);
-            var entWhite = this.computeEntropy(this.white, 9);
-            var entRed = this.computeEntropy(this.red, 9);
-            var entBlue = this.computeEntropy(this.blue, 9);
+            var entWhite = this.computeEntropy(this.white, 729);
+            var entRed = this.computeEntropy(this.red, 729);
+            var entBlue = this.computeEntropy(this.blue, 729);
             var entA1 = this.computeEntropy(this.a1, 8);
             var entA2 = this.computeEntropy(this.a2, 8);
             var entA3 = this.computeEntropy(this.a3, 8);
@@ -66,26 +67,32 @@ var J = {};
             return b;
         },
         computeEntropy:function(tab, n){
-            var tabTest = [1,1,1,1,1,1,1,1,1,1,1,1,1,0];
             var entropy = 0;
             var value = 0;
             var cpt = 0;
             for(var i = 0;i <= n;i++){
                 cpt = 0;
-                for(var j = 0;j < tabTest.length;j++){
-                    if(tabTest[j] === i){
+                for(var j = 0;j < tab.length;j++){
+                    if(tab[j] === i){
                         cpt = cpt + 1;
                     }
                 }
                 if(cpt !== 0){
-                    value = cpt / tabTest.length;
+                    value = cpt / tab.length;
                     entropy = entropy -  value*(Math.log(value)/Math.log(2));
                 }
            }
-           alert(entropy);
            return entropy;
         },
         pushValue:function(a1,a2,a3){
+            if (this.white.length === this.TAB_MAX) {
+                this.white = this.white.slice(1);
+                this.red = this.red.slice(1);
+                this.blue = this.blue.slice(1);
+                this.a1 = this.a1.slice(1);
+                this.a2 = this.a2.slice(1);
+                this.a3 = this.a3.slice(1);
+            }
             this.white.push(this.env.white);
             this.red.push(this.env.red);
             this.blue.push(this.env.blue);
